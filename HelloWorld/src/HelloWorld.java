fff54fb0362a981d8205e8775f395c46f2b01544
LucasEhret <LucasEhret@0-208-03>
2020-09-18 12:52:01 +0200

    Zählspiel #1

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,13 +1,19 @@
-
+//Dies ist ein Zählspiel//
 public class HelloWorld {
 
+	static int counter = 0;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World! This is my first line. I like cake, now.");
-		System.out.println("Yeaaaahhhhh");
-		System.out.println("One hour and a half till it´s weekend");
-		System.out.println("Marc sitzt neben mir");
-				
+	
+		
+		
+		while(counter<100) {
+			if(counter%5==0) {System.out.println("Hello Bäm!");}
+				else {System.out.println("Hello World!");}
+			System.out.println(counter);
+			counter++;
+			 
+		}
 	}
 
 }
