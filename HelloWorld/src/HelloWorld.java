8abd03ae73eaf5b3b88c28fd8ca5cf3474501f4c
LucasEhret <LucasEhret@0-208-03>
2020-09-18 11:56:11 +0200

    Initial commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! This is my first line. I like cake, now.");
+		System.out.println("Yeaaaahhhhh");
+		System.out.println("One hour and a half till it´s weekend");
+		System.out.println("Marc sitzt neben mir");
+				
+	}
+
+}
