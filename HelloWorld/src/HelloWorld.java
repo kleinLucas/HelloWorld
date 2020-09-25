6c906df7d044cc004d5444ce2c54c710ab3e5926
LucasEhret <LucasEhret@0-208-03>
2020-09-25 11:59:41 +0200

    FIZZBUZZ

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,19 +1,32 @@
 //Dies ist ein Zählspiel//
 public class HelloWorld {
 
-	static int counter = 0;
+	static int counter = 1;
 	
 	public static void main(String[] args) {
 	
 		
 		
-		while(counter<100) {
-			if(counter%2==0) {System.out.println("Hello Space!");}
-				else {System.out.println("Hello World!");}
-			System.out.println(counter);
+		while(counter<=100) {
+		  if(counter%3==0 && counter%5==0) {
+			  System.out.println("FizzBuzz");}
+		  else {
+			  if(counter%3==0) {System.out.println("Fizz");}
+				else {
+				  if(counter%5==0) {System.out.println("Buzz");}
+				    else {System.out.println(counter);} 
+						
+						
+				}
+		  }
+			
 			counter++;
 			 
 		}
+		
 	}
+	}
+	
 
-}
+
+
